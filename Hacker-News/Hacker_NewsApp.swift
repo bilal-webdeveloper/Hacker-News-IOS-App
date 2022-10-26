c16dae6769aa78600f633c3331a027ee0fbc7e31
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-10-26 18:07:26 +0200

    Initial Commit

---
+++ Hacker-News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker-News
+//
+//  Created by Bilal Dallali on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
